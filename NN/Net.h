26de4ae552d8a833288f89e4d1edd2c64a15bd35
XiaopeiXu <xvxiaopeisjtu@gmail.com>
2016-03-14 18:22:21 -0500

    update complete

--- NN/Net.h
+++ NN/Net.h
@@ -7,13 +7,16 @@ private:
 	int numOfL;       //number of layers
 	vector<Layer> layers;
 public:
+	static double learningRate;
 	void printNet();
 	Net(const vector<int> net);
 	void getResults(vector<double> &res);
-	void forward(vector<double> &tuple);
-	void backward(vector<double> &res);
+	void forward(vector<double> &tuple);  //use input to get output
+	void backward(vector<double> &res);   //use output and target to get ¦Ä
+	void update();                        //use ¦Ä to update weights
 
 };
+double Net::learningRate = 0.05;
 
 void Net::printNet()
 {
@@ -85,17 +88,17 @@ void Net::forward(vector<double> &tuple)
 
 void Net::backward(vector<double> &res)
 {
-	cout<<"Layer "<<numOfL-1<<" : "<<endl;
+	//cout<<"Layer "<<numOfL-1<<" : "<<endl;
 	for(int i=1;i<layers[numOfL-1].nodes.size();i++)  //output layer
 	{
 		double o=layers[numOfL-1].nodes[i].output;
 		layers[numOfL-1].nodes[i].delta=o*(1-o)*(res[i-1]-o);
-		cout<<layers[numOfL-1].nodes[i].delta<<endl;
+		//cout<<layers[numOfL-1].nodes[i].delta<<endl;
 	}
 
 	for(int i=layers.size()-2;i>=0;i--)  //for each layer,hidden
 	{
-		cout<<"Layer "<<i<<" : "<<endl;
+		//cout<<"Layer "<<i<<" : "<<endl;
 		for(int h=1;h<layers[i].nodes.size();h++)
 		{
 			double o=layers[i].nodes[h].output;
@@ -105,9 +108,26 @@ void Net::backward(vector<double> &res)
 				sum+=layers[i+1].nodes[k].delta*layers[i+1].nodes[k].inputWs[h];
 			}
 			layers[i].nodes[h].delta=o*(1-o)*sum;
-			cout<<layers[i].nodes[h].delta<<endl;
+			//cout<<layers[i].nodes[h].delta<<endl;
 		}
 	}
+}
+
+
+void Net::update()
+{
+	for(int i=1;i<layers.size();i++)         //every layer
+	{
+		for(int j=1;j<layers[i].nodes.size();j++)      //every node 
+		{
+			for(int k=0;k<layers[i].nodes[j].inputWs.size();k++)   //every weight
+			{
+				double dw=learningRate*layers[i].nodes[j].delta*layers[i-1].nodes[k].output;
+				layers[i].nodes[j].inputWs[k]+=dw;
+			}
+		}
+	}
+
 }
 
 #endif 
